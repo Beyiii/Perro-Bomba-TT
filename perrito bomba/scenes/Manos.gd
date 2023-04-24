32554c4ba9deff77341e5ad19bc20bcd99abb10e
CatalinaLC <lopezcatalina17@gmail.com>
2023-04-23 20:13:27 -0400

    Cambio manos

--- perrito bomba/scenes/Manos.gd
+++ perrito bomba/scenes/Manos.gd
@@ -36,6 +36,8 @@ var Enemy = preload("res://scenes/enemy.tscn")
 @onready var manitoDer = $"Manito der"
 @onready var manitoIzq = $"Manito izq"
 
+enum MANOS {AMBAS, IZQ, DER}
+var mano_actual = MANOS.AMBAS
 
 var click_manoDer = false
 var click_manoIzq = true
@@ -99,6 +101,8 @@ func _process(delta):
 		if click_manoDer == true and click_manos == false:
 			playback_der.travel("CerrarDer")
 			
+			
+			
 	#Cierra los dos primeros dedos
 	if Input.is_action_pressed("Dedo_1") and Input.is_action_pressed("Dedo_2") and !(Input.is_action_pressed("Dedo_3") or Input.is_action_pressed("Dedo_4")):
 		if click_manoIzq == true and click_manos == false:
@@ -122,6 +126,8 @@ func _process(delta):
 			
 		if click_manoDer == true and click_manos == false:
 			playback_der.travel("DerechosCerrarDer")
+			
+			
 		
 	if click_manos == true:
 		self.position = self.position.move_toward(mouse_pos, 400 * delta)
@@ -144,82 +150,6 @@ func _process(delta):
 	
 
 	
-	# animation
-	"""
-	if is_on_floor():
-		if abs(velocity.x) > 10 or move_input:
-			playback.travel("run")
-		else:
-			playback.travel("idle")
-	else:
-		if velocity.y < 0:
-			playback.travel("jump")
-		else:
-			playback.travel("fall")	
+
 		
 		
-	"""
-
-
-"""
-
-
-func _ready():
-	animation_tree.active = true
-#	Engine.time_scale = 0.2
-
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-	if is_on_floor():
-		current_airborne_time = 0
-
-	if Input.is_action_just_pressed("jump") and current_airborne_time < MAX_AIRBORNE_TIME:
-		jumping = true
-		current_jump_time = 0
-		Game.jumps += 1
-	
-	if jumping and current_jump_time <= MAX_JUMP_TIME:
-		velocity.y = JUMP_VELOCITY
-	
-	current_jump_time += delta
-	current_airborne_time += delta
-	
-	if Input.is_action_just_released("jump"):
-		jumping = false
-	
-
-	var move_input = Input.get_axis("move_left", "move_right")
-	
-	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
-
-	move_and_slide()
-
-	if Input.is_action_just_pressed("spawn"):
-		_spawn()
-	
-	# animation
-	
-	if is_on_floor():
-		if abs(velocity.x) > 10 or move_input:
-			playback.travel("run")
-		else:
-			playback.travel("idle")
-	else:
-		if velocity.y < 0:
-			playback.travel("jump")
-		else:
-			playback.travel("fall")
-	
-	if move_input:
-		pivot.scale.x = sign(move_input)
-
-
-func _spawn():
-	var enemy = Enemy.instantiate()
-	enemy.global_position = get_global_mouse_position()
-	enemy.rotation = (get_global_mouse_position() - global_position).angle() + PI / 2
-	get_parent().add_child(enemy)
-
-""" 
