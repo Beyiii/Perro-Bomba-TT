591a260f1bf141631bd8e760bf6b1882649e452d
Manu123-321 <manuel.aceituno@ug.uchile.cl>
2023-06-03 16:11:19 -0400

    creditos en progreso

---
+++ perrito bomba/Creditos.gd
@@ -0,0 +1,36 @@
+extends Node2D
+@onready var timer = $Timer
+@onready var label1 = %"Titulos creditos"
+@onready var label2 = %"Contendio creditos"
+
+
+var indice = 0
+
+var titulos_creditos = ['Creditos',
+	'Música',
+	'otrotexto'
+]
+var contenido_creditos = ['','Linkin Park \nRihanna \nPerro Chocolo', '' ] 
+var titulos = 'Creditos'
+var contenido = ''
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.set_wait_time(2)
+	timer.start()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if indice < len(titulos_creditos):
+		label1.set_text(titulos_creditos[indice])
+		label2.set_text(contenido_creditos[indice])
+	else:
+		get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+
+
+func _on_timer_timeout():
+		indice += 1
--- perrito bomba/ui/main_menu.gd
+++ perrito bomba/ui/main_menu.gd
@@ -4,6 +4,8 @@ extends MarginContainer
 @onready var exit = %Exit
 @onready var tutorial = %Tutorial
 @onready var ajustes = %Ajustes
+@onready var creditos = %Creditos
+
 
 @export var main_scene: PackedScene
 
@@ -12,6 +14,7 @@ func _ready():
 	tutorial.pressed.connect(_on_tutorial_pressed)
 	exit.pressed.connect(_on_exit_pressed)
 	ajustes.pressed.connect(_on_ajustes_pressed)
+	creditos.pressed.connect(_on_creditos_pressed)
 	
 func _on_start_pressed():
 #	get_tree().change_scene_to_file("res://scenes/main.tscn")
@@ -27,3 +30,6 @@ func _on_tutorial_pressed():
 # oli
 func _on_ajustes_pressed():
 	get_tree().change_scene_to_file("res://scenes/ajustes.tscn")
+
+func _on_creditos_pressed():
+	get_tree().change_scene_to_file("res://scenes/creditos.tscn")
